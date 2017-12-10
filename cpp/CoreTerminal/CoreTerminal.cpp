8179e0105928b7934322dacc84bf93df04585ac7
Jonny Archer <me@jonnyarcher.com>
2017-12-10 14:05:12 -0600

    Proper form
    Moves the actual funcions into the cpp file, and leaves the rest to the header.

--- cpp/CoreTerminal/CoreTerminal.cpp
+++ cpp/CoreTerminal/CoreTerminal.cpp
@@ -5,5 +5,98 @@
 //  Created by Jonathan Archer on 10/12/17.
 //
 
+#include <CoreVideo.h>
+#include <CoreTerminal.h>
 
 unsigned int OpenedTerminal;
+
+extern unsigned int video_colorcode;
+extern unsigned int video_position;
+
+extern int j;
+
+extern char *vidptr;
+
+    void CoreTerminal::CommandError(const char * str) {
+        video_colorcode = 0x04;
+        CoreVideo.PrintOne("[ ERROR ] ");
+        video_colorcode = 0x07;
+        j = 0;
+        while(str[j] != '\0') {                             // While not terminating character
+
+            if (str[j] == '\n') { CoreVideo.Newline(); j++; }
+            else {
+                vidptr[video_position] = str[j];
+                vidptr[video_position+1] = video_colorcode;     // Color attribute
+                ++j;                                            // Increment
+                video_position = video_position + 2;            // Also increment, count for attribute byte
+            }
+        }
+        return;
+    }
+    void CoreTerminal::OpenShell(void) {
+        video_colorcode = 0x02;
+        CoreVideo.Print("> ");
+        if(video_position >= 3840) { CoreVideo.Scroll(); }
+        video_colorcode = 0x07;
+        OpenedTerminal = video_position;
+        return;
+    }
+
+    void CoreTerminal::RunCommand(char * Command) {
+
+      j = 0;
+      while(Command[j] == ' ') j++;
+
+      if((Command[j] == 's' || Command[j] == 'S') &&          // SCROLL
+         (Command[j+1] == 'c' || Command[j+1] == 'C') &&
+         (Command[j+2] == 'r' || Command[j+2] == 'R') &&
+         (Command[j+3] == 'o' || Command[j+3] == 'O') &&
+         (Command[j+4] == 'l' || Command[j+4] == 'L') &&
+         (Command[j+5] == 'l' || Command[j+5] == 'L') &&
+           (Command[j+6] == '\0')) {
+            CoreVideo.Scroll();
+            CoreVideo.Newline();
+            return;
+        }
+
+        if((Command[j] == 'c' || Command[j] == 'C') &&          // CLEAR
+           (Command[j+1] == 'l' || Command[j+1] == 'L') &&
+           (Command[j+2] == 'e' || Command[j+2] == 'E') &&
+           (Command[j+3] == 'a' || Command[j+3] == 'A') &&
+           (Command[j+4] == 'r' || Command[j+4] == 'R') &&
+           (Command[j+5] == '\0')) {
+            CoreVideo.ClearConsole();
+            return;
+        }
+
+	      if((Command[j] == 'e' || Command[j] == 'E') &&          // ECHO
+           (Command[j+1] == 'c' || Command[j+1] == 'C') &&
+           (Command[j+2] == 'h' || Command[j+2] == 'H') &&
+           (Command[j+3] == 'o' || Command[j+3] == 'O') &&
+           (Command[j+4] == ' ')) {
+			   CoreVideo.Newline();
+                if(video_position >= 3840) { CoreVideo.Scroll(); }
+				j = 5;
+				while(Command[j] != '\0') {                             // While not terminating character
+					vidptr[video_position] = Command[j];
+					vidptr[video_position+1] = video_colorcode;     // Color attribute
+					++j;                                            // Increment
+					video_position = video_position + 2;            // Also increment, count for attribute byte
+			}
+				CoreVideo.Newline();
+				return;
+        }
+
+		if(Command[j] == '\0') {        // Nothing does nothing
+			CoreVideo.Newline();
+            return;
+        }
+
+        CoreVideo.Print("\n");
+        CommandError("Unknown command '");
+        CoreVideo.Print(Command);
+        CoreVideo.PrintLn("'\n");
+        video_position = video_position - 160;
+        return;
+    }
--- cpp/CoreTerminal/CoreTerminal.h
+++ cpp/CoreTerminal/CoreTerminal.h
@@ -9,8 +9,6 @@
 #define CoreTerminal_h
 #endif /* CoreTerminal_h */
 
-#include "CoreVideo.h"
-
 extern unsigned int video_colorcode;
 extern unsigned int video_position;
 
@@ -23,86 +21,9 @@ class CoreTerminal {
 
 public:
 
-    void CommandError(const char * str) {
-        video_colorcode = 0x04;
-        CoreVideo.PrintOne("[ ERROR ] ");
-        video_colorcode = 0x07;
-        j = 0;
-        while(str[j] != '\0') {                             // While not terminating character
-
-            if (str[j] == '\n') { CoreVideo.Newline(); j++; }
-            else {
-                vidptr[video_position] = str[j];
-                vidptr[video_position+1] = video_colorcode;     // Color attribute
-                ++j;                                            // Increment
-                video_position = video_position + 2;            // Also increment, count for attribute byte
-            }
-        }
-        return;
-    }
-    void OpenShell(void) {
-        video_colorcode = 0x02;
-        CoreVideo.Print("> ");
-        if(video_position >= 3840) { CoreVideo.Scroll(); }
-        video_colorcode = 0x07;
-        OpenedTerminal = video_position;
-        return;
-    }
-
-    void RunCommand(char * Command) {
-
-        if((Command[0] == 's' || Command[0] == 'S') &&          // SCROLL
-           (Command[1] == 'c' || Command[1] == 'C') &&
-           (Command[2] == 'r' || Command[2] == 'R') &&
-           (Command[3] == 'o' || Command[3] == 'O') &&
-           (Command[4] == 'l' || Command[4] == 'L') &&
-           (Command[5] == 'l' || Command[5] == 'L') &&
-           (Command[6] == '\0')) {
-            CoreVideo.Scroll();
-            CoreVideo.Newline();
-            return;
-        }
-
-        if((Command[0] == 'c' || Command[0] == 'C') &&          // CLEAR
-           (Command[1] == 'l' || Command[1] == 'L') &&
-           (Command[2] == 'e' || Command[2] == 'E') &&
-           (Command[3] == 'a' || Command[3] == 'A') &&
-           (Command[4] == 'r' || Command[4] == 'R') &&
-           (Command[5] == '\0')) {
-            CoreVideo.ClearConsole();
-            return;
-        }
-
-	    if((Command[0] == 'e' || Command[0] == 'E') &&          // ECHO
-           (Command[1] == 'c' || Command[1] == 'C') &&
-           (Command[2] == 'h' || Command[2] == 'H') &&
-           (Command[3] == 'o' || Command[3] == 'O') &&
-           (Command[4] == ' ')) {
-			   CoreVideo.Newline();
-                if(video_position >= 3840) { CoreVideo.Scroll(); }
-				j = 5;
-				while(Command[j] != '\0') {                             // While not terminating character
-					vidptr[video_position] = Command[j];
-					vidptr[video_position+1] = video_colorcode;     // Color attribute
-					++j;                                            // Increment
-					video_position = video_position + 2;            // Also increment, count for attribute byte
-			}
-				CoreVideo.Newline();
-				return;
-        }
-
-		if(Command[0] == '\0') {        // Nothing does nothing
-			CoreVideo.Newline();
-            return;
-        }
-
-        CoreVideo.Print("\n");
-        CommandError("Unknown command '");
-        CoreVideo.Print(Command);
-        CoreVideo.PrintLn("'\n");
-        video_position = video_position - 160;
-        return;
-    }
+    void CommandError(const char * str);
+    void OpenShell(void);
+    void RunCommand(char * Command);
 
 };
 
